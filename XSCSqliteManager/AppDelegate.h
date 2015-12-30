fe66fbf494d3085f1a66cdf1e920bdd4bf273f8b
sancaixu <xsc18229467792@163.com>
2015-12-30 14:02:15 +0800

    Initial Commit

---
+++ XSCSqliteManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XSCSqliteManager
+//
+//  Created by 徐三才 on 15/12/30.
+//  Copyright © 2015年 sancaixu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XSCSqliteManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XSCSqliteManager
+//
+//  Created by 徐三才 on 15/12/30.
+//  Copyright © 2015年 sancaixu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
